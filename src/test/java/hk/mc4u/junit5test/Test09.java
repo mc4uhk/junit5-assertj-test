e0ded2ee365a9e7b9aaa8475ede00b3ce4bae8b5
Edmund Yau <mc4u.hk@gmail.com>
2021-08-27 23:37:52 +0800

    updated

---
+++ src/test/java/hk/mc4u/junit5test/Test09.java
@@ -0,0 +1,15 @@
+package hk.mc4u.junit5test;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Test09 {
+	@Test
+	public void test01() throws Exception {
+
+
+		
+	}
+}
